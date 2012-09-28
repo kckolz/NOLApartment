94126ec17ca925a3e49ebf4b8eaaf2df6a885d2f
Hunter Haydel <haydh530@gmail.com>
2012-09-27 23:52:58 -0500

    another address formating test

--- spec/ad_spec.rb
+++ spec/ad_spec.rb
@@ -13,10 +13,15 @@ describe Ad do
     @ad.address.should eq '209 Pine St, Metairie, LA'
   end
 
-  it 'should format address as street0 at street1, city, state if there is no house number in street0' do
+  it 'should format address as "street0 at street1, city, state" if there is no house number in street0' do
     @ad.street0 = 'Pine St'
-    @ad.street1 = 'Lake Ave'
 
     @ad.address.should eq 'Pine St at Lake Ave, Metairie, LA'
+  end
+
+  it 'should format address as "street0 at street1, city, state" if street0 is a number street with no house number' do
+    @ad.street0 = '1st St'
+
+    @ad.address.should eq '1st St at Lake Ave, Metairie, LA'
   end
 end
