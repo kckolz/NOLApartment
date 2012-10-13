d587e0553b8fd0fa0f28eef9cde1299903a5095b
Hunter Haydel <haydh530@gmail.com>
2012-10-13 12:26:07 -0500

    dont use google search without and api key and cut down on how often the config gets loaded

--- geocoder.rb
+++ geocoder.rb
@@ -1,10 +1,19 @@
 require 'geocoder'
 require 'open-uri'
 require 'json'
+require 'yaml'
 
 module NOLApartment
   module Geocoder
+
+    def self.google_key
+      @@google_key ||= YAML::load(File.new('config/keys.yaml').read)[:google]
+    end
+
     def self.get_location address, deep=true
+      # Can't do 'deep' searches without a google api key.
+      deep = deep && self.google_key != 'KEY'
+
       geocode_result = ::Geocoder.search(address).first
       result = {}
 
@@ -17,8 +26,7 @@ module NOLApartment
         if bad_result && deep
           # didn't get a good result. Google HALP!
           # TODO pull the google search out.
-          google_api_key = YAML::load(File.new('config/keys.yaml').read)[:google]
-          response = open "https://www.googleapis.com/customsearch/v1?key=#{google_api_key}&cx=008774535076880730414:pil9ab_j8-e&q=#{URI::encode(address)}"
+          response = open "https://www.googleapis.com/customsearch/v1?key=#{self.google_key}&cx=008774535076880730414:pil9ab_j8-e&q=#{URI::encode(address)}"
           search = JSON::parse response.read
 
           if spelling = search["spelling"]
